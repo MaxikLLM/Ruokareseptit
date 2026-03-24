ccc47c290ce46a5294b1319e0f37f262a1a99c80
Max <maksim.pronin.mp@gmail.com>
2026-03-24 23:43:11 +0200

    Show recipe list/info

--- recipes.py
+++ recipes.py
@@ -4,3 +4,17 @@ def add_recipe(title, ingredients, instruction, user_id):
     sql = "INSERT INTO recipes (title, ingredients, instruction, user_id) VALUES (?, ?, ?, ?)"
 
     db.execute(sql, [title, ingredients, instruction, user_id])
+    
+def get_recipes():
+    sql = "SELECT id, title FROM recipes ORDER BY id DESC"
+    return db.query(sql)
+    
+def get_recipe(recipe_id):
+    sql = """SELECT recipes.title,
+                    recipes.ingredients,
+                    recipes.instruction,
+                    users.username
+             FROM recipes, users
+             WHERE recipes.user_id = users.id AND
+                   recipes.id = ?"""
+    return db.query(sql, [recipe_id])[0]
